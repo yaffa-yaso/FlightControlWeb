558c5de886b30a097da4cd48e8b749334394c9ba
yaffa <yaffa@DESKTOP-VRQ5167>
2020-05-31 23:14:06 +0300

    update

--- FlightControlWeb/Models/FlightPlan.cs
+++ FlightControlWeb/Models/FlightPlan.cs
@@ -1,20 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FlightControlWeb.Models
 {
     public class FlightPlan
     {
+        [JsonProperty, JsonPropertyName("passengers")]
         public int passengers { get; set; }
-
+        
+        [JsonProperty, JsonPropertyName("company_name")]
         public string company_name { get; set; }
-
+        
+        [JsonProperty, JsonPropertyName("initial_location")]
         public InitialLocation initial_location { get; set; }
-
+        
+        [JsonProperty, JsonPropertyName("segments")]
         public IEnumerable<Segment> segments { get; set; }
-
+        
+        [JsonProperty, JsonPropertyName("date_time")]
         public string date_time { get; set; }
 
 
